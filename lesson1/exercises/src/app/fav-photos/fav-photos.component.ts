379758c374e275bb5a9e77664eef75450d011f2a
Kirby <kafracas@gmail.com>
2020-09-10 15:10:25 -0500

    updated styling

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn.shopify.com/s/files/1/0267/2679/3252/files/Limes_Fine_Art_copy_360x.jpg?v=1590365842';
+  image3 = 'https://c.stocksy.com/a/kGE900/z9/2199806.jpg';
 
   constructor() { }
 
