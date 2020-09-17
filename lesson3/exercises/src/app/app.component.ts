40196efab1dcd8d032217d88b13b1f24fd5f1995
Kirby <kafracas@gmail.com>
2020-09-17 16:20:06 -0500

    worked on lesson 3 exercise

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,69 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    this.takeOffEnabled = false;
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+    }
+ }
+
+ handleLand(rocketImage) {
+  let result = window.confirm('Are you sure the shuttle is ready to land?');
+  rocketImage.style.bottom = '0px';
+  this.takeOffEnabled = true;
+  if (result) {
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Shuttle has landed.';
+  }
+}
+
+handleAbortMission(rocketImage) {
+  let result = window.confirm('Are you sure you would like to abort the mission?');
+  rocketImage.style.bottom = '0px';
+  this.takeOffEnabled = true;
+  if (result) {
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Mission aborted.';
+  }
+}
+
+moveRocket(rocketImage, direction) {
+  this.warning();
+  if (direction === 'right') {
+  let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+  rocketImage.style.left = movement;
+  this.width = this.width + 10000;
+  } else if (direction === 'left') {
+    let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width = this.width - 10000;
+  } else if (direction === 'up') {
+    let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height + 10000;
+  } else if (direction === 'down') {
+    let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height - 10000;
+  }
+}
+
+warning() {
+  if (this.width === 0 || this.height === 0) {
+    this.color = 'orange'
+  }
+}
+
 }
